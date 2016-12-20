694512e5d1beb3304de0c8db18dccb691b17e2f6
marco <marco.palkoska@stud.hslu.ch>
2016-12-20 17:44:25 +0100

    reply to chat instead of user id to support groups. error handling on invalid input.

--- src/main.js
+++ src/main.js
@@ -18,11 +18,11 @@ const bot = new teleBot(token);
 * Sends the user a welcome message.
 */
 bot.on('/start', msg => {
-  let fromId = msg.from.id;
+  let chatId = msg.chat.id;
   let firstName = msg.from.first_name;
   let reply = msg.message_id;
   let text = msg.text;
-  return bot.sendMessage(fromId, `Welcome, ${ firstName }!`, { reply });
+  return bot.sendMessage(chatId, `Welcome, ${ firstName }!\nUse /help to see what this bot is all about.`, { reply });
 });
 
 /*
@@ -30,11 +30,11 @@ bot.on('/start', msg => {
 * Sends the user a help message with a list of all available commands.
 */
 bot.on('/help', msg => {
-  let fromId = msg.from.id;
+  let chatId = msg.chat.id;
   let username = msg.from.username;
-  console.log(" => send help.txt to user #" + fromId + " : " + username);
+  console.log(" => send help.txt to user #" + chatId + " : " + username);
   var helpmessage = fs.readFileSync('src/text/help.txt').toString();
-  return bot.sendMessage(fromId, helpmessage);
+  return bot.sendMessage(chatId, helpmessage);
 });
 
 /*
@@ -42,12 +42,12 @@ bot.on('/help', msg => {
 * Sends the brainfuck command list to the user.
 */
 bot.on('/brainfuck', msg => {
-  let fromId = msg.from.id;
+  let chatId = msg.chat.id;
   let username = msg.from.username;
-  console.log(" => send brainfuck_info.txt to user #" + fromId + " : " + username);
+  console.log(" => send brainfuck_info.txt to user #" + chatId + " : " + username);
   var infoMessage = fs.readFileSync('src/text/brainfuck_info.txt').toString();
   // TODO: use markdown to make commands bold
-  return bot.sendMessage(fromId, infoMessage);
+  return bot.sendMessage(chatId, infoMessage);
 });
 
 /*
@@ -55,10 +55,10 @@ bot.on('/brainfuck', msg => {
 * Removes all clutter from the received code and returns plain brainfuck code.
 */
 bot.on('/minify', msg => {
-  let fromId = msg.from.id;
+  let chatId = msg.chat.id;
   let brainfuckCode = minify(msg.text);
-  bot.sendMessage(fromId, 'Minified code:');
-  return bot.sendMessage(fromId, brainfuckCode);
+  bot.sendMessage(chatId, 'Minified code:');
+  return bot.sendMessage(chatId, brainfuckCode);
 });
 
 /*
@@ -66,10 +66,22 @@ bot.on('/minify', msg => {
 * Parses the brainfuck code and returns the output to the user as ASCII characters.
 */
 bot.on('/run', msg => {
-  let fromId = msg.from.id;
+  let chatId = msg.chat.id;
   let brainfuckCode = minify(msg.text);
-  let text = brainfuck.text(brainfuckCode);
-  return bot.sendMessage(fromId, text);
+
+  // check if there is any brianfuck code in the input.
+  if (brainfuckCode) {
+    let text = brainfuck.text(brainfuckCode);
+
+    // check if the output contains any displayable characters.
+    if (text && /\s/.test(text)) {
+      return bot.sendMessage(chatId, text);
+    } else {
+      return bot.sendMessage(chatId, "Error! Your code did not produce any output. Use /debug to see memory values.");
+    }
+  } else {
+    return bot.sendMessage(chatId, "Error! You did not provide any brainfuck code. Use /brainfuck to get more information.")
+  }
 });
 
 /*
@@ -77,10 +89,10 @@ bot.on('/run', msg => {
 * Parses the brainfuck code and returns the output to the user as decimal numbers.
 */
 bot.on('/debug', msg => {
-  let fromId = msg.from.id;
+  let chatId = msg.chat.id;
   let brainfuckCode = minify(msg.text);
   let data = brainfuck(brainfuckCode);
-  return bot.sendMessage(fromId, data.toString());
+  return bot.sendMessage(chatId, data.toString());
 });
 
 /*
@@ -90,7 +102,8 @@ bot.on('/debug', msg => {
 function minify(code) {
   // Regex selects all characters that are relevant for brainfuck: < > . , [ ] - +
   // the match() returns an array which is converted into a string by the join()-method with no sepparator.
-  let minifiedCode = code.match(/(<|>|\.|,|\[|\]|-|\+)/g).join('');
+  let minifiedCodeArray = code.match(/(<|>|\.|,|\[|\]|-|\+)/g);
+  let minifiedCode = minifiedCodeArray ? minifiedCodeArray.join('') : null;
   console.log(` => minified code: ${ minifiedCode }`);
   return minifiedCode;
 }
